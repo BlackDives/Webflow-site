1b3673dcd11c39382526d1d168e40538c42c2067
BlackDives <sirkamronwilson00@gmail.com>
2021-11-26 12:37:30 -0600

    checking

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Flex>
+      <Text>Hello World</Text>
+    </Flex>
+  );
+};
+
+export default Navbar;
